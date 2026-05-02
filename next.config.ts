ffc5674b83c801dcc030ee0f021f69153756df15
Eduardo Espinosa <eduespinosa@users.noreply.github.com>
2026-05-01 19:02:41 -0700

    Changed NextConfig

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: __dirname,
+  /* config options here */
 };
 
 export default withNextIntl(nextConfig);
